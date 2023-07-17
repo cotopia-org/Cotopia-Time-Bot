ac1d67135a561fc46a0f1b92e4d9c8f176ad847e
Ali Kharrati <ali.kharrati@gmail.com>
2023-07-17 14:29:30 +0330

    cleaning up

--- main.py
+++ main.py
@@ -27,34 +27,17 @@ def run():
     
     @bot.event
     async def on_message(message):
-        guild = message.guild
-        # print(guild)
         if message.author == bot.user:
             return
 
-        # if message.content.startswith(":zombie:"):
-            # await message.author.move_to(None, reason="You have been reported a zombie and didn't respond!")
-            # await guild.kick(message.author, reason="You have been reported a zombie and didn't respond!")
-            # async for member in guild.fetch_members(limit=150):
-            #     print(member.name)
-            #     await message.channel.send(member)
+        
 
     @bot.event
     async def on_voice_state_update(member, before, after):
-
-        # print("before:  "+ str(before))
-        # print("after:  "+ str(after))
-
         log_processor.record(member, before, after)
         raw_logger.record(member, before, after)
 
-        # m = member.guild.get_member(member.id)
-        # print("is_on_mobile():    " + str(m.is_on_mobile()))
-        # print("raw_status:    " + m.raw_status)
-        # print("status:    " + str(m.status))
-        # print("web_status:    " + str(m.web_status))
-        # print("desktop_status:    " + str(m.desktop_status))
-        # print("mobile_status:    " + str(m.mobile_status))
+       
 
     @bot.event
     async def on_presence_update(before, after):
@@ -96,18 +79,6 @@ def run():
             await ctx.send("Well well you are not a zombie " + member.mention + "!")
 
 
-
-
-    # @bot.hybrid_command()
-    # async def week0(ctx):
-    #     await ctx.send(file=discord.File("./plogs/reports/LunaBot🌙#9997.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/ajabimahdi.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/armanhr.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/imebneali.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/kharrati.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/m.habibi.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/mamreez_tn#5785.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/navid.madadi.txt"))
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
--- test.py
+++ test.py
@@ -8,3 +8,36 @@ print(report.make_report("kharrati",0 ,1689424634))
 
 # datetime_object = datetime.fromtimestamp(1689424634)
 # print(datetime_object)
+
+
+
+
+    # @bot.hybrid_command()
+    # async def week0(ctx):
+    #     await ctx.send(file=discord.File("./plogs/reports/LunaBot🌙#9997.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/ajabimahdi.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/armanhr.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/imebneali.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/kharrati.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/m.habibi.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/mamreez_tn#5785.txt"))
+    #     await ctx.send(file=discord.File("./plogs/reports/navid.madadi.txt"))
+
+
+    # if message.content.startswith(":zombie:"):
+            # await message.author.move_to(None, reason="You have been reported a zombie and didn't respond!")
+            # await guild.kick(message.author, reason="You have been reported a zombie and didn't respond!")
+            # async for member in guild.fetch_members(limit=150):
+            #     print(member.name)
+            #     await message.channel.send(member)
+
+
+ # m = member.guild.get_member(member.id)
+        # print("is_on_mobile():    " + str(m.is_on_mobile()))
+        # print("raw_status:    " + m.raw_status)
+        # print("status:    " + str(m.status))
+        # print("web_status:    " + str(m.web_status))
+        # print("desktop_status:    " + str(m.desktop_status))
+        # print("mobile_status:    " + str(m.mobile_status))
+
+        
