7e1511ce4ca743036858e697da8cc155c5bb4294
Ali Kharrati <ali.kharrati@gmail.com>
2023-07-17 12:35:55 +0330

    ADD add_duration_column.py

--- add_duration_column.py
+++ add_duration_column.py
@@ -1,4 +1,13 @@
+#
+#
+#
+# This should be run manually, for updating the old database
+
+
+
+
 import psycopg2
+import asyncio
 
 
 async def main():
@@ -8,8 +17,24 @@ async def main():
 
     cur.execute("ALTER TABLE discord_event ADD COLUMN IF NOT EXISTS duration INTEGER Default -1;")
 
-    
+    cur.execute("""
+                SELECT * From discord_event
+                WHERE pairid IS NOT NULL
+                AND kind = ANY(ARRAY['SESSION STARTED', 'TALKING STARTED', 'SESSION PAUSED'])
+                ORDER BY id
+                """)
+    all_rows = cur.fetchall()
+
+    for start in all_rows:
+        cur.execute("SELECT * From discord_event WHERE id = %s;", [start[6]])
+        end = cur.fetchone()
+        duration = end[2] - start[2]
+        print(str(start[0]) + " -------> " + str(start[2]) + "||" + str(end[2]) + "||" + str(duration))
+        cur.execute("UPDATE discord_event SET duration = %s WHERE id = %s", (duration, start[0]))
 
     conn.commit()
     cur.close()
     conn.close()
+
+
+asyncio.run(main())
