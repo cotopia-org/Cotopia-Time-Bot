35b6d40096e874555976aba4450df9fac65564e9
Ali Kharrati <ali.kharrati@gmail.com>
2023-07-18 21:17:13 +0330

    ADD test for update

--- test.py
+++ test.py
@@ -1,77 +1,16 @@
 import psycopg2
-import report
-import time
-import datetime
-import asyncio
 
+conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                        password="Tp\ZS?gfLr|]'a", port=5432)
+cur = conn.cursor()
 
-# def epoch_generator(year: int, month: int, day: int):
-#     ts= datetime.datetime(year=year, month=month, day=day, hour=0, minute=0).strftime('%s')
-#     return ts
+cur.execute("""
+                SELECT * From discord_event
+                WHERE epoch > 1689702404
+                ORDER BY id
+                """)
 
+data = cur.fetchall()
 
-# print(epoch_generator(2023, int("07"), int("07")))
-
-# datetime_object = datetime.fromtimestamp(1689424634)
-# print(datetime_object)
-
-
-
-
-    # @bot.hybrid_command()
-    # async def week0(ctx):
-    #     await ctx.send(file=discord.File("./plogs/reports/LunaBot🌙#9997.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/ajabimahdi.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/armanhr.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/imebneali.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/kharrati.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/m.habibi.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/mamreez_tn#5785.txt"))
-    #     await ctx.send(file=discord.File("./plogs/reports/navid.madadi.txt"))
-
-
-    # if message.content.startswith(":zombie:"):
-            # await message.author.move_to(None, reason="You have been reported a zombie and didn't respond!")
-            # await guild.kick(message.author, reason="You have been reported a zombie and didn't respond!")
-            # async for member in guild.fetch_members(limit=150):
-            #     print(member.name)
-            #     await message.channel.send(member)
-
-
- # m = member.guild.get_member(member.id)
-        # print("is_on_mobile():    " + str(m.is_on_mobile()))
-        # print("raw_status:    " + m.raw_status)
-        # print("status:    " + str(m.status))
-        # print("web_status:    " + str(m.web_status))
-        # print("desktop_status:    " + str(m.desktop_status))
-        # print("mobile_status:    " + str(m.mobile_status))
-
-
-
-# guild = ctx.guild
-#         view = discord.ui.View()
-#         async for member in guild.fetch_members(limit=150):
-#             b = discord.ui.Button(label=str(member))
-#             view.add_item(b)
-        
-#         await ctx.send(view=view)
-
-
-
-# def today():
-#         the_string = datetime.datetime.today().strftime('%Y-%m-%d')
-#         slices = the_string.split("-")
-#         dic = {"y": int(slices[0]), "m": int(slices[1]), "d": int(slices[2])}
-#         return dic
-
-# print(today())
-
-dic1 = {"channel": "e"}
-
-dic2 = {"key": 12}
-
-dic1 = dic1 | dic2
-
-print(dic1)
-
-
+for row in data:
+    print(row)
