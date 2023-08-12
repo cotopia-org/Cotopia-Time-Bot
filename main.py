fa00f30892793a0ffbd24f2da1df441a342f4aca
Ali Kharrati <ali.kharrati@gmail.com>
2023-08-12 09:23:17 +0330

    ADD /thismonth report

--- main.py
+++ main.py
@@ -367,8 +367,6 @@ def run():
 
         log_processor.renew_pendings()
 
-
-
         now = today_jalali()
         start_epoch = int(
                 JalaliDateTime(
@@ -383,13 +381,12 @@ def run():
         
         the_board = report.make_board(start_epoch, end_epoch)
 
-        discordDate_from = JalaliDateTime.fromtimestamp(start_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
-        discordDate_to = JalaliDateTime.fromtimestamp(end_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
+        title_date = JalaliDate.fromtimestamp(start_epoch)
+        # discordDate_to = JalaliDateTime.fromtimestamp(end_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
 
-        text = ("Net Session Hours\n" +
-        "From: " + discordDate_from + "\n" +
-        "To: " + discordDate_to + "\n" +
-        "------------------------------\n")
+        text = ("Net Session Hours of " +
+        str(title_date) +
+        "\n------------------------------\n")
         for l in the_board:
             string = str(l)
             string = string.replace("('", "")
@@ -417,13 +414,12 @@ def run():
         
         the_board = report.make_board(start_epoch, end_epoch)
 
-        discordDate_from = JalaliDateTime.fromtimestamp(start_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
-        discordDate_to = JalaliDateTime.fromtimestamp(end_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
+        title_date = JalaliDate.fromtimestamp(start_epoch)
+        # discordDate_to = JalaliDateTime.fromtimestamp(end_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
 
-        text = ("Net Session Hours\n" +
-        "From: " + discordDate_from + "\n" +
-        "To: " + discordDate_to + "\n" +
-        "------------------------------\n")
+        text = ("Net Session Hours of " +
+        str(title_date) +
+        "\n------------------------------\n")
         for l in the_board:
             string = str(l)
             string = string.replace("('", "")
@@ -432,6 +428,39 @@ def run():
             text = text + string + "\n"
 
         await ctx.send(text)
+    
+    @bot.hybrid_command(description="جدول مدت سشن های این ماه")
+    async def thismonth(ctx):
+        now = today_jalali()
+        start_epoch = int(
+                JalaliDateTime(
+                    year=now["y"], month=now["m"], day=1, hour=0, minute=0, second=0,
+                    tzinfo=pytz.timezone("Asia/Tehran")).to_gregorian().strftime('%s')
+        )
+        end_epoch = int(
+                JalaliDateTime(
+                    year=now["y"], month=now["m"], day=now["d"], hour=23, minute=59, second=59,
+                    tzinfo=pytz.timezone("Asia/Tehran")).to_gregorian().strftime('%s')
+        )
+        
+        the_board = report.make_board(start_epoch, end_epoch)
+
+        title_date = JalaliDate.fromtimestamp(start_epoch).strftime("%Y/%m")
+        # discordDate_to = JalaliDateTime.fromtimestamp(end_epoch, pytz.timezone("Asia/Tehran")).strftime("%c")
+
+        text = ("Net Session Hours of " +
+        str(title_date) +
+        "\n------------------------------\n")
+        for l in the_board:
+            string = str(l)
+            string = string.replace("('", "")
+            string = string.replace("',", " :")
+            string = string.replace(")", "")
+            text = text + string + "\n"
+
+        await ctx.send(text)
+
+
 
 
 
