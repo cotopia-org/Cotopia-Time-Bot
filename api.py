f8ab41716e39c654b57f50398738d44624efae96
Ali Kharrati <ali.kharrati@gmail.com>
2023-09-19 11:19:57 +0330

    CORS ADD 8787

--- api.py
+++ api.py
@@ -36,8 +36,12 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8787",
+    "http://127.0.0.1:8787",
     "http://localhost:5173/",
     "http://127.0.0.1:5173/",
+    "http://localhost:8787/",
+    "http://127.0.0.1:8787/",
     "http://localhost:8080/",
     "http://127.0.0.1:8080/",
 ]
