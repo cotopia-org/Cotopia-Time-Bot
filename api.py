f7930e4b5686e0e89afde0345435ab2daa2569d1
Ali Kharrati <ali.kharrati@gmail.com>
2023-09-19 11:17:02 +0330

    CORS port 8080

--- api.py
+++ api.py
@@ -38,8 +38,8 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5173/",
     "http://127.0.0.1:5173/",
-    "http://localhost:8000/",
-    "http://127.0.0.1:8000/",
+    "http://localhost:8080/",
+    "http://127.0.0.1:8080/",
 ]
 
 app.add_middleware(
