9e4f60d28a4b36db5467c12ed01e39fcf96a8be7
Ali Kharrati <ali.kharrati@gmail.com>
2023-09-28 23:23:29 +0330

    CHANGE required to False in my settings modal

--- person/my_settings_modal.py
+++ person/my_settings_modal.py
@@ -19,12 +19,14 @@ class MySettingsModal(discord.ui.Modal, title="Settings"):
         style = discord.TextStyle.short,
         label = "Your email",
         placeholder = "me@gmail.com",
+        required = False
     )
 
     trc20_wallet_addr = discord.ui.TextInput(
         style = discord.TextStyle.short,
         label = "Your TRC20 Wallet Address",
-        placeholder = " TRC2034c276example45DaE400ecA3deCc"
+        placeholder = " TRC2034c276example45DaE400ecA3deCc",
+        required = False
     )
 
     async def on_submit(self, interaction: discord.Interaction):
