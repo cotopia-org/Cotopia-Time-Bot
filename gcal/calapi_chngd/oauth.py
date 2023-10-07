080975f73ff56f0998265a397345e0a4d7d373a4
Ali Kharrati <ali.kharrati@gmail.com>
2023-10-07 12:47:41 +0330

    CHANGE google_token expiry format

--- gcal/calapi_chngd/oauth.py
+++ gcal/calapi_chngd/oauth.py
@@ -103,6 +103,6 @@ class Oauth(object):
             'client_id': flow.credentials.client_id,
             'client_secret': flow.credentials.client_secret,
             'scopes': flow.credentials.scopes,
-            'expiry': str(flow.credentials.expiry)
+            'expiry': flow.credentials.expiry.strftime('%Y-%m-%dT%H:%M:%S')
         }
         return self.__session_credentials
