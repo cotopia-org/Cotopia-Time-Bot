c0b56c0de9d92736f35539fb1cd247b46a3ba465
Ali Kharrati <ali.kharrati@gmail.com>
2023-10-20 12:20:17 +0330

    ADD driver schemas

---
+++ driver/schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from datetime import datetime, timezone, time, date
+
+class OurBaseModel(BaseModel):
+        class Config:
+            json_encoders = {
+                datetime: lambda v: v.astimezone(timezone.utc).isoformat(),
+                time: lambda v: v.astimezone(timezone.utc).isoformat(),
+                date: lambda v: v.astimezone(timezone.utc).isoformat(),
+            }
+
+
+class DriverBase(OurBaseModel):
+    email: str
+    username: str
+
+
+class DriverCreate(DriverBase):
+    password: str
+
+
+class DriverUpdate(DriverCreate):
+    password: str | None = None
+    location: str | None = None
+    timezone: str | None = None
+    locale: str | None = None
+    other_settings: str | None = None
+
+class Driver(DriverUpdate):
+    id: int
+    created_at: datetime
+    roles: str
+    class Config:
+        orm_mode = True
+
+class MinimalDriver(DriverBase):
+    class Config:
+        orm_mode = True
