f9db785ed8ec12a01517e368acd4fe326283b5e4
Ali Kharrati <ali.kharrati@gmail.com>
2024-02-08 18:35:20 +0330

    ADD create_db() for postgresql

---
+++ board/create_db.py
@@ -0,0 +1,37 @@
+import psycopg2
+
+
+def create_db():
+    conn = psycopg2.connect(
+        host="localhost",
+        dbname="postgres",
+        user="postgres",
+        password="Tp\ZS?gfLr|]'a",
+        port=5432,
+    )
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """   CREATE TABLE IF NOT EXISTS dirooz_boards(
+                                guild_id BIGINT NOT NULL,
+                                channel_id BIGINT NOT NULL,
+                                msg_id BIGINT NOT NULL,
+                                last_update BIGINT NOT NULL); """)
+    
+    print("dirooz_boards created @postgres")
+
+    cursor.execute(
+        """   CREATE TABLE IF NOT EXISTS inmaah_boards(
+                                guild_id BIGINT NOT NULL,
+                                channel_id BIGINT NOT NULL,
+                                msg_id BIGINT NOT NULL,
+                                last_update BIGINT NOT NULL); """)
+    
+    print("inmaah_boards created @postgres")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+create_db()
