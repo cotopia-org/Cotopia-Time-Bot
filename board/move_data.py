c61adaad42888c52120df7571e83eb16a6a24d19
Ali Kharrati <ali.kharrati@gmail.com>
2024-02-08 18:35:45 +0330

    ADD copy_to_postgres() from sqlite data

---
+++ board/move_data.py
@@ -0,0 +1,40 @@
+# copy data from sqlite tables to postgres tables
+
+import sqlite3
+
+import psycopg2
+
+
+tables = ["dirooz_boards", "inmaah_boards"]
+
+
+
+def copy_to_postgres(table_name: str):
+    conn = sqlite3.connect("timeboards.db")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    data = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+    conn = psycopg2.connect(
+        host="localhost",
+        dbname="postgres",
+        user="postgres",
+        password="Tp\ZS?gfLr|]'a",
+        port=5432,
+    )
+    cursor = conn.cursor()
+
+    for i in data:
+        cursor.execute(f"INSERT INTO {table_name} VALUES{i}")
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+for each in tables:
+    copy_to_postgres(each)
