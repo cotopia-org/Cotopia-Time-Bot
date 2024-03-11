fba86f69967c05a8f52ca7ab46f261a9b5d2deab
Katerou22 <xkaterou22@gmail.com>
2024-03-11 16:30:03 +0330

    Refactoring raw sqls to Model.

--- board/the_text.py
+++ board/the_text.py
@@ -7,6 +7,7 @@ from persiantools.jdatetime import JalaliDate, JalaliDateTime, timedelta
 
 import log_processor
 import report
+from models.Models import InMaahBoard, DiroozBoard
 
 
 # returns epoch of NOW: int
@@ -45,11 +46,11 @@ async def gen_dirooz_board(guild):
     updated_on = JalaliDateTime.now().strftime("%H:%M")
 
     text = (
-        "LEADERBOARD OF "
-        + str(title_date)
-        + "\n`updated on "
-        + updated_on
-        + "`\n------------------------------\n"
+            "LEADERBOARD OF "
+            + str(title_date)
+            + "\n`updated on "
+            + updated_on
+            + "`\n------------------------------\n"
     )
     for line in the_board:
         text = text + str(line[1]) + " | " + line[0] + "\n"
@@ -57,30 +58,34 @@ async def gen_dirooz_board(guild):
     # send the text
     msg = await da_channel.send(text + "‌")
 
-    # record to db
-    conn = psycopg2.connect(
-        host="localhost",
-        dbname="discord_bot_db",
-        user="cotopia",
-        password="123123",
-        port=5432,
-    )
-    cursor = conn.cursor()
-    cursor.execute(
-        """   CREATE TABLE IF NOT EXISTS dirooz_boards(
-                                guild_id BIGINT NOT NULL,
-                                channel_id BIGINT NOT NULL,
-                                msg_id BIGINT NOT NULL,
-                                last_update BIGINT NOT NULL); """
-    )
-    cursor.execute(f"DELETE FROM dirooz_boards WHERE guild_id = {guild.id};")
-    cursor.execute(
-        f"""     INSERT INTO dirooz_boards VALUES
-                                ({guild.id}, {da_channel.id}, {msg.id}, {rightnow()});"""
-    )
-    conn.commit()
-    cursor.close()
-    conn.close()
+    # # record to db
+    # conn = psycopg2.connect(
+    #     host="localhost",
+    #     dbname="discord_bot_db",
+    #     user="cotopia",
+    #     password="123123",
+    #     port=5432,
+    # )
+    # cursor = conn.cursor()
+    # cursor.execute(
+    #     """   CREATE TABLE IF NOT EXISTS dirooz_boards(
+    #                             guild_id BIGINT NOT NULL,
+    #                             channel_id BIGINT NOT NULL,
+    #                             msg_id BIGINT NOT NULL,
+    #                             last_update BIGINT NOT NULL); """
+    # )
+
+    # cursor.execute(f"DELETE FROM dirooz_boards WHERE guild_id = {guild.id};")
+    DiroozBoard.delete().where(guild_id=guild.id).execute()
+
+    DiroozBoard.create(guild_id=guild.id, channel_id=da_channel.id, msg_id=msg.id, last_update=rightnow())
+    # cursor.execute(
+    #     f"""     INSERT INTO dirooz_boards VALUES
+    #                             ({guild.id}, {da_channel.id}, {msg.id}, {rightnow()});"""
+    # )
+    # conn.commit()
+    # cursor.close()
+    # conn.close()
 
 
 async def update_dirooz_board(guild):
@@ -94,10 +99,11 @@ async def update_dirooz_board(guild):
     )
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM dirooz_boards WHERE guild_id = {guild.id};")
-    db_msg = cursor.fetchone()
+    dirooz_row = DiroozBoard.get(DiroozBoard.guild_id == guild.id)
+    # db_msg = cursor.fetchone()
 
     # check if it's outdated
-    last_update = JalaliDate.fromtimestamp(db_msg[3])
+    last_update = JalaliDate.fromtimestamp(dirooz_row.last_update)
 
     if JalaliDate.today().day == last_update.day:
         print("no need to update dirooz board")
@@ -107,8 +113,8 @@ async def update_dirooz_board(guild):
         return
     else:
         # get the msg object
-        channel = guild.get_channel(db_msg[1])
-        message = await channel.fetch_message(db_msg[2])
+        channel = guild.get_channel(dirooz_row.channel_id)
+        message = await channel.fetch_message(dirooz_row.msg_id)
 
         # make the board
         dirooz = JalaliDate.today() - timedelta(days=1)
@@ -129,11 +135,11 @@ async def update_dirooz_board(guild):
         updated_on = JalaliDateTime.now().strftime("%H:%M")
 
         text = (
-            "LEADERBOARD OF "
-            + str(title_date)
-            + "\n`updated on "
-            + updated_on
-            + "`\n------------------------------\n"
+                "LEADERBOARD OF "
+                + str(title_date)
+                + "\n`updated on "
+                + updated_on
+                + "`\n------------------------------\n"
         )
         for line in the_board:
             text = text + str(line[1]) + " | " + line[0] + "\n"
@@ -141,12 +147,14 @@ async def update_dirooz_board(guild):
         # send edit
         await message.edit(content=text + "‌")
         # update db
-        cursor.execute(
-            f"UPDATE dirooz_boards SET last_update = {rightnow()} WHERE guild_id = {guild.id};"
-        )
-        conn.commit()
-        cursor.close()
-        conn.close()
+
+        DiroozBoard.update(last_update=rightnow()).where(guild_id=guild.id).execute()
+        # cursor.execute(
+        #     f"UPDATE dirooz_boards SET last_update = {rightnow()} WHERE guild_id = {guild.id};"
+        # )
+        # conn.commit()
+        # cursor.close()
+        # conn.close()
 
 
 async def gen_inmaah_board(guild):
@@ -194,11 +202,11 @@ async def gen_inmaah_board(guild):
     updated_on = JalaliDateTime.now().strftime("%H:%M")
 
     text = (
-        "LEADERBOARD OF "
-        + str(title_date)
-        + "\n`updated on "
-        + updated_on
-        + "`\n------------------------------\n"
+            "LEADERBOARD OF "
+            + str(title_date)
+            + "\n`updated on "
+            + updated_on
+            + "`\n------------------------------\n"
     )
     for line in the_board:
         text = text + str(line[1]) + " | " + line[0] + "\n"
@@ -227,6 +235,9 @@ async def gen_inmaah_board(guild):
         f"""     INSERT INTO inmaah_boards VALUES
                                 ({guild.id}, {da_channel.id}, {msg.id}, {rightnow()});"""
     )
+
+    # InMaahBoard.create(guild_id=guild.id,channel_id=da_channel.id,msg_id=msg.id,last_update=rightnow())
+
     conn.commit()
     cursor.close()
     conn.close()
@@ -293,11 +304,11 @@ async def update_inmaah_board(guild):
         updated_on = JalaliDateTime.now().strftime("%H:%M")
 
         text = (
-            "LEADERBOARD OF "
-            + str(title_date)
-            + "\n`updated on "
-            + updated_on
-            + "`\n------------------------------\n"
+                "LEADERBOARD OF "
+                + str(title_date)
+                + "\n`updated on "
+                + updated_on
+                + "`\n------------------------------\n"
         )
         for line in the_board:
             text = text + str(line[1]) + " | " + line[0] + "\n"
