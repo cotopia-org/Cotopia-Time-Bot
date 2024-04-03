2efb0bda861f43abd3a2543280c4e901c610ca82
Taban Kharrati <tabankharrati@gmail.com>
2024-04-03 11:55:01 +0330

    CHANGE /today command to work with tz and calsys

--- commands/daily_report.py
+++ commands/daily_report.py
@@ -85,47 +85,42 @@ async def dirooz(ctx):
     await ctx.send(text)
 
 
-@commands.hybrid_command(description="Session durations of current day")
+@commands.hybrid_command(description="Session durations of the current day")
 async def today(ctx):
     log_processor.renew_pendings(driver=str(ctx.guild.id))
-
-    today = datetime.date.today()
     person = Person()
-    tz = person.get_timezone(discord_guild=ctx.guild.id, discord_id=ctx.author.id)
+    locale = person.get_locale(discord_guild=ctx.guild.id, discord_id=ctx.author.id)
+    tz = locale["timezone"]
+    calsys = locale["cal_system"]
 
-    start_dt = datetime.datetime(year=today.year, month=today.month, day=today.day)
-    localized_start_dt = pytz.timezone(tz).localize(dt=start_dt)
-    start_epoch = int(localized_start_dt.timestamp())
-
-    end_dt = datetime.datetime(
-        year=today.year,
-        month=today.month,
-        day=today.day,
-        hour=23,
-        minute=59,
-        second=59,
-    )
-    localized_end_dt = pytz.timezone(tz).localize(dt=end_dt)
-    end_epoch = int(localized_end_dt.timestamp()) + 1
+    now = datetime.datetime.now(pytz.timezone(tz))
+    start_dt = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    start_epoch = int(start_dt.timestamp())
+    end_epoch = start_epoch + (24 * 3600)
 
     the_board = report.make_board(
         driver=str(ctx.guild.id), start_epoch=start_epoch, end_epoch=end_epoch
     )
 
-    title_date = datetime.date.fromtimestamp(start_epoch)
-
-    # discordDate_to = JalaliDateTime.fromtimestamp(end_epoch, pytz.timezone(tz)).strftime("%c")
+    if calsys == "Gregorian":
+        title_date = datetime.date.fromtimestamp(start_epoch)
+    elif calsys == "Jalali":
+        title_date = JalaliDate.fromtimestamp(start_epoch)
 
     text = (
-        "Net Session Hours of " + str(title_date) + "\n------------------------------\n"
+        "Net Session Hours of `"
+        + str(title_date)
+        + "`\n`tz: "
+        + tz
+        + "`\n------------------------------\n"
     )
     for i in the_board:
         text = text + str(i[1]) + " | <@" + i[0] + ">\n"
-
+    
     await ctx.send(text)
 
 
-@commands.hybrid_command(description="Session durations of last day")
+@commands.hybrid_command(description="Session durations of the previous day")
 async def yesterday(ctx):
     yesterday = datetime.date.today() - timedelta(days=1)
     person = Person()
