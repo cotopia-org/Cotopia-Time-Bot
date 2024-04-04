a01558a6638c1eb045f7b6aa526b1459d480d0b2
Taban Kharrati <tabankharrati@gmail.com>
2024-04-04 13:52:54 +0330

    CHANGE refactor /make_board command to work with tz and calsys

--- commands/make_board.py
+++ commands/make_board.py
@@ -2,7 +2,6 @@ import datetime
 from typing import Optional
 
 import pytz
-from discord import Member
 from discord.ext import commands
 from persiantools.jdatetime import JalaliDateTime
 
@@ -11,103 +10,149 @@ import report
 from person import Person
 
 
-@commands.hybrid_command()
+@commands.hybrid_command(
+    description="Generates servers leader board in a given period. Default date is current month."
+)
 async def make_board(
     ctx,
-    start_ssss: typing.Optional[int] = 1349,
-    start_mm: typing.Optional[int] = 1,
-    start_rr: typing.Optional[int] = 1,
-    end_ssss: typing.Optional[int] = 1415,
-    end_mm: typing.Optional[int] = 12,
-    end_rr: typing.Optional[int] = 29,
+    start_yyyy: Optional[int] = 4141,
+    start_mm: Optional[int] = 1,
+    start_dd: Optional[int] = 1,
+    end_yyyy: Optional[int] = 4242,
+    end_mm: Optional[int] = 12,
+    end_dd: Optional[int] = 29,
 ):
-    emrooz = JalaliDate.today()
     person = Person()
-    tz = person.get_timezone(discord_guild=ctx.guild.id, discord_id=ctx.author.id)
+    locale = person.get_locale(discord_guild=ctx.guild.id, discord_id=ctx.author.id)
+    tz = locale["timezone"]
+    calsys = locale["cal_system"]
 
-    # I want to set today as default end value, but passing it in Args didnt work. So I do this:
-    if end_ssss == 1415 and end_mm == 12 and end_rr == 29:
-        end_dt = JalaliDateTime(
-            year=emrooz.year,
-            month=emrooz.month,
-            day=emrooz.day,
-            hour=23,
-            minute=59,
-            second=59,
-        )
-        localized_end_dt = pytz.timezone(tz).localize(dt=end_dt)
-        end_epoch = int(localized_end_dt.timestamp()) + 1
+    if calsys == "Gregorian":
+        now = datetime.datetime.now(pytz.timezone(tz))
+    elif calsys == "Jalali":
+        now = JalaliDateTime.now(pytz.timezone(tz))
+    # print(now)
+
+    # I wanted to set now as default end value, but passing it in Args didnt work. So I do this:
+    if end_yyyy == 4242 and end_mm == 12 and end_dd == 29:
+        end_epoch = int(now.timestamp()) + 1
     else:
         try:
-            end_dt = JalaliDateTime(
-                year=end_ssss,
-                month=end_mm,
-                day=end_rr,
-                hour=23,
-                minute=59,
-                second=59,
-            )
-            localized_end_dt = pytz.timezone(tz).localize(dt=end_dt)
-            end_epoch = int(localized_end_dt.timestamp()) + 1
+            if calsys == "Gregorian":
+                user_input = datetime.datetime(
+                    year=end_yyyy,
+                    month=end_mm,
+                    day=end_dd,
+                    hour=23,
+                    minute=59,
+                    second=59,
+                )
+                user_input = pytz.timezone(tz).localize(user_input)
+                # print(f"END user input: {user_input}")
+                end_epoch = int(user_input.timestamp()) + 1
+            elif calsys == "Jalali":
+                user_input = JalaliDateTime(
+                    year=end_yyyy,
+                    month=end_mm,
+                    day=end_dd,
+                    hour=23,
+                    minute=59,
+                    second=59,
+                )
+                user_input = pytz.timezone(tz).localize(user_input)
+                # print(f"END user input: {user_input}")
+                end_epoch = int(user_input.timestamp()) + 1
         except:  # noqa: E722
-            await ctx.send("Please enter a valid date!", ephemeral=True)
+            await ctx.send(
+                f"Please enter a valid end date!\nYour calendar system is {calsys}.",
+                ephemeral=True,
+            )
             return
 
-    if start_ssss == 1349 and start_mm == 1 and start_rr == 1:
-        start_dt = JalaliDateTime(year=emrooz.year, month=emrooz.month, day=1)
-        localized_start_dt = pytz.timezone(tz).localize(dt=start_dt)
-        start_epoch = int(localized_start_dt.timestamp())
+    # I wanted to set start of the month as default end value, but passing it in Args didnt work. So I do this:
+    if start_yyyy == 4141 and start_mm == 1 and start_dd == 1:
+        start_dt = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        start_epoch = int(start_dt.timestamp())
     else:
         try:
-            start_dt = JalaliDateTime(year=start_ssss, month=start_mm, day=start_rr)
-            localized_start_dt = pytz.timezone(tz).localize(dt=start_dt)
-            start_epoch = int(localized_start_dt.timestamp())
+            if calsys == "Gregorian":
+                user_input = datetime.datetime(
+                    year=start_yyyy,
+                    month=start_mm,
+                    day=start_dd,
+                )
+                user_input = pytz.timezone(tz).localize(user_input)
+                # print(f"START user input: {user_input}")
+                start_epoch = int(user_input.timestamp())
+            elif calsys == "Jalali":
+                user_input = JalaliDateTime(
+                    year=start_yyyy,
+                    month=start_mm,
+                    day=start_dd,
+                )
+                user_input = pytz.timezone(tz).localize(user_input)
+                # print(f"START user input: {user_input}")
+                start_epoch = int(user_input.timestamp())
         except:  # noqa: E722
-            await ctx.send("Please enter a valid date!", ephemeral=True)
+            await ctx.send(
+                f"Please enter a valid start date!\nYour calendar system is {calsys}.",
+                ephemeral=True,
+            )
             return
 
     if int(start_epoch) >= int(end_epoch):
         await ctx.send(
-            "**Start Date** should be before **End Date**! Try Again!",
+            f"**Start Date** should be before **End Date**! Try Again!\nYour calendar system is {calsys}.",
             ephemeral=True,
         )
         return
     # max int value in postgres is 	-2147483648 to 2147483647
     elif int(end_epoch) > 2147400000:
         await ctx.send(
-            "**End Date** is too far in the future! Try Again!", ephemeral=True
+            f"**End Date** is too far in the future! Try Again!\nYour calendar system is {calsys}.",
+            ephemeral=True,
         )
         return
     elif int(start_epoch) < 0:
-        await ctx.send("I wasn't even born back then! Try Again!", ephemeral=True)
+        await ctx.send(
+            f"I wasn't even born back then! Try Again!\nYour calendar system is {calsys}.",
+            ephemeral=True,
+        )
         return
 
-    print("start epoch: " + str(start_epoch))
-    print("end epoch: " + str(end_epoch))
+    # print("start epoch: " + str(start_epoch))
+    # print("end epoch: " + str(end_epoch))
 
     log_processor.renew_pendings(driver=str(ctx.guild.id))
     the_board = report.make_board(
         driver=str(ctx.guild.id), start_epoch=start_epoch, end_epoch=end_epoch
     )
 
-    discordDate_from = JalaliDateTime.fromtimestamp(
-        int(start_epoch), pytz.timezone(tz)
-    ).strftime("%c")
-    discordDate_to = JalaliDateTime.fromtimestamp(
-        int(end_epoch), pytz.timezone(tz)
-    ).strftime("%c")
+    if calsys == "Gregorian":
+        reportDate_from = datetime.datetime.fromtimestamp(
+            start_epoch, tz=pytz.timezone(tz)
+        ).strftime("%Y/%m/%d")
+        reportDate_to = datetime.datetime.fromtimestamp(
+            end_epoch, tz=pytz.timezone(tz)
+        ).strftime("%Y/%m/%d")
+    elif calsys == "Jalali":
+        reportDate_from = JalaliDateTime.fromtimestamp(
+            start_epoch, tz=pytz.timezone(tz)
+        ).strftime("%Y/%m/%d")
+        reportDate_to = JalaliDateTime.fromtimestamp(
+            end_epoch, tz=pytz.timezone(tz)
+        ).strftime("%Y/%m/%d")
 
     text = (
-        "Net Session Hours\n" + "From:  " + str(discordDate_from) + "\n"
-        "To:  " + str(discordDate_to) + "\n------------------------------\n"
+        "Net Session Hours\n" + "From:  `" + str(reportDate_from) + "`\n"
+        "To:  `"
+        + str(reportDate_to)
+        + "`\n`tz: "
+        + tz
+        + "`\n------------------------------\n"
     )
     for i in the_board:
         text = text + str(i[1]) + " | <@" + i[0] + ">\n"
-        # string = str(i)
-        # string = string.replace("('", "")
-        # string = string.replace("',", " :")
-        # string = string.replace(")", "")
-        # text = text + string + "\n"
 
     await ctx.send(text)
 
