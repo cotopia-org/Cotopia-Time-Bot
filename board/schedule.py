bbf71c929dbe4e0775488e0f5dc4c472b7867ee4
Taban Kharrati <tabankharrati@gmail.com>
2024-07-19 07:22:33 +0330

    CHANGE get_schedules result to str keys

--- board/schedule.py
+++ board/schedule.py
@@ -7,7 +7,7 @@ def get_schedules(start_epoch: int, end_epoch: int, guild_id: int):
     result = {}
     REQUEST_URL = "http://tooljet.cotopia.social:8084/total_hours/all"
     start_str = datetime.datetime.fromtimestamp(start_epoch).strftime("%Y-%m-%d")
-    end_str = datetime.datetime.fromtimestamp(end_epoch).strftime("%Y-%m-%d")
+    end_str = datetime.datetime.fromtimestamp(end_epoch - 1).strftime("%Y-%m-%d")
     params = {
         "id_server": guild_id,
         "start_date": start_str,
@@ -17,7 +17,7 @@ def get_schedules(start_epoch: int, end_epoch: int, guild_id: int):
     if r.status_code == 200:
         the_list = r.json()["total_duration_hours"]
         for each in the_list:
-            result[int(each["id_discord"])] = each["total_duration_hours"]
+            result[each["id_discord"]] = each["total_duration_hours"]
     else:
         print("http://tooljet.cotopia.social:8084/total_hours/all RETURNED AN ERROR")
         print(r.json())
